c2bddd23c9284a175f27305848a9e942d9247767
ganiushina <32090008+ganiushina@users.noreply.github.com>
2019-12-02 15:23:11 +0400

    first commit

--- app/src/main/java/ru/geekbrains/gb_android_libraries/MainView.java
+++ app/src/main/java/ru/geekbrains/gb_android_libraries/MainView.java
@@ -6,7 +6,5 @@ import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 @StateStrategyType(value = AddToEndSingleStrategy.class)
 public interface MainView extends MvpView {
-    void setButton1Text(int value);
-    void setButton2Text(int value);
-    void setButton3Text(int value);
+    void setButtonText(int index, int value);
 }
