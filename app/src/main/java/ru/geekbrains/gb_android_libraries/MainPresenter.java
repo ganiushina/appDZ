dec23c84c18d99e04951480890c39286ff6c5cb9
ganiushina <32090008+ganiushina@users.noreply.github.com>
2019-12-05 16:24:47 +0400

    Добавила Observer и PublishSubject

--- app/src/main/java/ru/geekbrains/gb_android_libraries/MainPresenter.java
+++ app/src/main/java/ru/geekbrains/gb_android_libraries/MainPresenter.java
@@ -1,32 +1,134 @@
 package ru.geekbrains.gb_android_libraries;
 
+import android.annotation.SuppressLint;
 
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
 
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import timber.log.Timber;
+
 @InjectViewState
 public class MainPresenter extends MvpPresenter<MainView> {
 
-    private Model model = new Model();
+    private Model model;
+    private FakeRepository repository;
 
+    final int[] val = new int[1];
+    final int[] val1 = new int[1];
+    final int[] val2 = new int[1];
+
+
+    public MainPresenter() {
+        this.model = new Model();
+        repository = new FakeRepository();
+    }
+
+    @SuppressLint("TimberArgCount")
     @Override
     protected void onFirstViewAttach() {
         super.onFirstViewAttach();
+        model.subscribe((message) -> {
+            if (message instanceof MainPresenter) {
+                Timber.v("Testing ", ((MainPresenter) message).val[0]+1);
+            }
+            if (message instanceof Integer) {
+                Timber.v("Testing ", message);
+            }
+        });
+    }
+    private void calcValue1(int index, int value){
+        model.setAt(index, value);
     }
 
-    private int calcValue(int index){
-        model.setAt(index, model.getAt(index) + 1);
-        return model.getAt(index);
-    }
+     public void counter1Click(int value){
+         calcValue1(value, val[0]);
+         model.publish(this, value);
+         model.getData(value)
+                 .observeOn(AndroidSchedulers.mainThread())
+                 .subscribe(new Observer<Integer>() {
+                     @Override
+                     public void onSubscribe(Disposable d) {
+                         Timber.d("onSubscribe");
+                     }
 
-    public void counter1Click(int value){
-        getViewState().setButton1Text(calcValue(value));
+                     @Override
+                     public void onNext(Integer value) {
+                         val[0] = value+1;
+                         getViewState().setButton1Text(val[0]);
+                         Timber.d(String.format("onNext111 %d", value));
+                     }
+
+                     @Override
+                     public void onError(Throwable e) {
+                         Timber.e(e, "onError");
+                     }
+
+                     @Override
+                     public void onComplete() {
+                         Timber.d("onComplete");
+                     }
+                 });
     }
 
     public void counter2Click(int value){
-        getViewState().setButton2Text(calcValue(value));
+        calcValue1(value, val1[0]);
+        model.getData(value)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Timber.d("onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(Integer value) {
+                        val1[0] = value+1;
+                        getViewState().setButton2Text(val1[0]);
+                        Timber.d(String.format("onNext111 %d", value));
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Timber.e(e, "onError");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Timber.d("onComplete");
+                    }
+                });
     }
+
     public void counter3Click(int value){
-        getViewState().setButton3Text(calcValue(value));
+        calcValue1(value, val2[0]);
+        model.getData(value)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Timber.d("onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(Integer value) {
+                        val2[0] = value+1;
+                        getViewState().setButton3Text(val2[0]);
+                        Timber.d(String.format("onNext111 %d", value));
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Timber.e(e, "onError");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Timber.d("onComplete");
+                    }
+                });
     }
+
 }
--- app/src/main/java/ru/geekbrains/gb_android_libraries/MainView.java
+++ app/src/main/java/ru/geekbrains/gb_android_libraries/MainView.java
@@ -4,6 +4,8 @@ import com.arellomobile.mvp.MvpView;
 import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
+import io.reactivex.Observable;
+
 @StateStrategyType(value = AddToEndSingleStrategy.class)
 public interface MainView extends MvpView {
     void setButton1Text(int value);
